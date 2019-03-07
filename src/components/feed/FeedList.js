61d3d4ec6e802975b5bf46a8baa99259ad33ba6a
LiJiansheng <leekinsung@gmail.com>
2019-03-07 19:44:27 +0800

    Feed Summary
    test git

--- src/components/feed/FeedList.js
+++ src/components/feed/FeedList.js
@@ -1,41 +1,13 @@
 import React from 'react'
+import FeedSummary from './FeedSummary'
 
 const FeedList=()=>{
 	return (
 		<div className="feed-list section">
 
-		<div className="card z-depth-0 project-summary">
-			<div className="card-content grey-text text-darken-3">
-				<span className="card-title">Feed Title</span>
-				<p>Posted By</p>
-				<p className="grey-text">01 March, 1pm</p>
-				</div>
-				</div>
+		<ProjectSummary />
 
-				<div className="card z-depth-0 project-summary">
-			<div className="card-content grey-text text-darken-3">
-				<span className="card-title">Feed Title</span>
-				<p>Posted By</p>
-				<p className="grey-text">01 March, 1pm</p>
-				</div>
-				</div>
-
-				<div className="card z-depth-0 project-summary">
-			<div className="card-content grey-text text-darken-3">
-				<span className="card-title">Feed Title</span>
-				<p>Posted By</p>
-				<p className="grey-text">01 March, 1pm</p>
-				</div>
-				</div>
-				<div className="card z-depth-0 project-summary">
-			<div className="card-content grey-text text-darken-3">
-				<span className="card-title">Feed Title</span>
-				<p>Posted By</p>
-				<p className="grey-text">01 March, 1pm</p>
-				</div>
-				</div>
-
-				</div>
+		</div>
 		)
 
 }

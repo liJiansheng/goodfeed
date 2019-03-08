866c2dd056e513dd7b69d5ce2297879a7a745459
LiJiansheng <leekinsung@gmail.com>
2019-03-08 14:49:26 +0800

    Update SignIn.js

--- src/components/auth/SignIn.js
+++ src/components/auth/SignIn.js
@@ -4,26 +4,32 @@ class SignIn extends Component {
 state={
 
 }
+handleChange= (e) =>{
 
+}
+
+handleSubmit=(e)=>{
+
+}
 render(){
 return(
 	<div className="container">
-			<form onSubmit=() className="white">
+			<form onSubmit={this.handleSubmit} className="white">
 			<h5 className="grey-text text-darken-3">Sign In</h5>
 			<div className="input-field">
 			<label htmlFor="email">Email</label>
-			<input type="email" id="email" onChange={}/>
+			<input type="email" id="email" onChange={this.handleChange}/>
 			</div>
 			<div className="input-field">
 			<label htmlFor="password">Password</label>
-			<input type="password" id="password" onChange={}/>
+			<input type="password" id="password" onChange={this.handleChange}/>
 			</div>
 			<div className="input-field">
 			<button className="btn pink lighten-1 z-depth-0">Login</button>
 			</div>
 			</form>
 			</div>
-			
+
 
 
 	)

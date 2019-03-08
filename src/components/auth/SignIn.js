2032b3c8b8d79b11c04ed3916ada8397b09686aa
LiJiansheng <leekinsung@gmail.com>
2019-03-08 14:59:14 +0800

    update state auth

--- src/components/auth/SignIn.js
+++ src/components/auth/SignIn.js
@@ -2,14 +2,18 @@ import React, { Component } from 'react'
 
 class SignIn extends Component {
 state={
-
+email:'',
+password:''
 }
 handleChange= (e) =>{
-
+this.setState({
+[e.target.id]: e.target.value
+})
 }
 
 handleSubmit=(e)=>{
-
+e.preventDefault();
+console.log(this.state)
 }
 render(){
 return(
@@ -30,10 +34,7 @@ return(
 			</form>
 			</div>
 
-
-
 	)
-
 }
-
 }
+export default SignIn

c41cd3bdf9405d6652092c3cc9226f1f552e800f
LiJiansheng <leekinsung@gmail.com>
2019-03-08 15:08:05 +0800

    sign up

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from './components/layout/Navbar'
 import Dashboard from './components/dashboard/Dashboard'
 import FeedDetails from './components/feed/FeedDetails'
 import SignIn from './components/auth/SignIn'
+import SignUp from './components/auth/SignUp'
 
 class App extends Component {
   render() {
@@ -15,6 +16,8 @@ class App extends Component {
         <Route exact path='/' component={Dashboard} />
          <Route path='/feed/:id' component={FeedDetails} />
          <Route path='/signin' component={SignIn} />
+          <Route path='/signup' component={SignUp} />
+
       </Switch>
       </div>
       </BrowserRouter>

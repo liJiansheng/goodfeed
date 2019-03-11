ceaa9eaf4c09271d4d3a59a2744a5d638133f9b7
LiJiansheng <leekinsung@gmail.com>
2019-03-11 11:40:16 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { creatStore } from 'redux'
+import { createStore } from 'redux'
 import rootReducer from './store/reducers/rootReducer'
 import { Provider } from 'react-redux'
 
-const store = creatStore(rootReducer);
+const store = createStore(rootReducer);
 
 ReactDOM.render(<Provider store ={store}><App /></Provider>, document.getElementById('root'));
 

8e039afdaf923cfe3f57e5a020405aa81237737d
LiJiansheng <leekinsung@gmail.com>
2019-03-12 08:25:23 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import * as serviceWorker from './serviceWorker';
 import { createStore,applyMiddleware, compose } from 'redux'
 import rootReducer from './store/reducers/rootReducer'
 import { Provider } from 'react-redux'
-import thunk from 'redu-thunk'
+import thunk from 'redux-thunk'
 import { reduxFirestore, getFirestore } from 'redux-firestore'
 import { reactReduxFirebase, getFirebase } from 'react-redux-firebase'
 import fbConfig from './config/fbConfig'

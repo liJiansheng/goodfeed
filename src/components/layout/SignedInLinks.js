7c40d6575d3e40aa65711f4da57199041958fc64
LiJiansheng <leekinsung@gmail.com>
2019-03-13 11:17:30 +0800

    Update SignedInLinks.js

--- src/components/layout/SignedInLinks.js
+++ src/components/layout/SignedInLinks.js
@@ -3,7 +3,7 @@ import { NavLink } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { signOut } from '../../store/actions/authActions'
 
-const SignedInLinks = () =>{
+const SignedInLinks = (props) =>{
 	return (
 		<ul className="right">
 		<li><NavLink to='/'>Write Feed</NavLink></li>

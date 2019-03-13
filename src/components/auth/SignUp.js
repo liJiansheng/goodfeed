1d0d9569717abdd1f0fe2600c23f03b962f0315f
LiJiansheng <leekinsung@gmail.com>
2019-03-13 21:14:39 +0800

    sign up

--- src/components/auth/SignUp.js
+++ src/components/auth/SignUp.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import {Redirect } from 'react-router-dom'
 import { connect} from 'react-redux'
+import { signUp } from '../../store/actions/authActions'
 
 class SignUp extends Component {
 state={
@@ -17,7 +18,7 @@ this.setState({
 
 handleSubmit=(e)=>{
 e.preventDefault();
-console.log(this.state)
+this.props.signUp(this.state)
 }
 render(){
 
@@ -55,6 +56,12 @@ render(){
 const mapStateToProps=(state)=>{
 	return{
 		auth:state.firebase.auth
+	}
+}
+
+const mapDispatchToProps=(dispatch)=>{
+	return{
+		signUp: (newUser)=>dispatch(signUp(newUser))
 	}
 }
 export default connect(mapStateToProps)(SignUp)
--- src/store/reducers/authReducer.js
+++ src/store/reducers/authReducer.js
@@ -17,6 +17,11 @@ const authReducer=(state=initState, action)=>{
 			}
 		case 'SIGNOUT_SUCCESS':
 			return state;
+		case 'SIGNUP_SUCCESS':
+			return {
+				...state,
+				authError:action.err.message
+			}
 		default:
 		return state;	
 	}

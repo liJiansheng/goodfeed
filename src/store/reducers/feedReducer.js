965dffe292bac4ec16f21243b20988a5d0fad648
LiJiansheng <leekinsung@gmail.com>
2019-03-26 16:58:16 +0800

    Update feedReducer.js

--- src/store/reducers/feedReducer.js
+++ src/store/reducers/feedReducer.js
@@ -1,4 +1,11 @@
+const initState={
+	feeds: [
+	{id:'1', title:'Day 1', message:'its a good day'},
+	{id:'2', title:'Day 2', message:'its a better day'},
+	{id:'3', title:'Day 3', message:'its a best day'},
+	]
 
+}
 
 const feedReducer=(state=initState, action)=>{
 

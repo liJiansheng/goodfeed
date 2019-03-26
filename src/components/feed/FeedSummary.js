b4247fad63230a691bf50fe7610f9c8e6dbe1b48
LiJiansheng <leekinsung@gmail.com>
2019-03-26 11:57:55 +0800

    update css

--- src/components/feed/FeedSummary.js
+++ src/components/feed/FeedSummary.js
@@ -3,13 +3,14 @@ import moment from 'moment'
 
 const FeedSummary=({feed})=>{
 	return(
-			<div className="card z-depth-0 project-summary">
+		<div className="card z-depth-0 project-summary">
 			<div className="card-content grey-text text-darken-3">
 				<span className="card-title">{feed.title}</span>
+				
 				<p>Posted By  {feed.authorFirstName} {feed.authorLastName}</p>
 				<p className="grey-text">{moment(feed.createdAt.toDate()).calendar()}</p>
-				</div>
-				</div>
+			</div>
+		</div>
 
 		)
 }

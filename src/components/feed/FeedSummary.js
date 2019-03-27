7c17e136b43f0192cd71432ae46756e0c9290758
LiJiansheng <leekinsung@gmail.com>
2019-03-27 13:24:38 +0800

    Update FeedSummary.js

--- src/components/feed/FeedSummary.js
+++ src/components/feed/FeedSummary.js
@@ -1,12 +1,10 @@
 import React from 'react'
 import moment from 'moment'
 
-
 const FeedSummary=({feed})=>{
 	return(
 		<div className="card z-depth-0 project-summary">
 			<div className="card-content grey-text text-darken-3">
-				<img src={ feed.feedImgURL }>
 				<span className="card-title">{feed.title}</span>
 				<p>Posted By  {feed.authorFirstName} {feed.authorLastName}</p>
 				<p className="grey-text">{moment(feed.createdAt.toDate()).calendar()}</p>

b710421555e8aa7fa149459fc0d9a609ce31efcb
LiJiansheng <leekinsung@gmail.com>
2019-04-01 13:30:14 +0800

    profile

--- src/components/dashboard/Notifications.js
+++ src/components/dashboard/Notifications.js
@@ -1,12 +1,28 @@
 import React from 'react'
+import { connect } from 'react-redux' 
 
-const Notifications =()=>{
+const Notifications =(props)=>{
+
+	const { profile } = props;
+
 	return(
-		<div>
-			<p>Notifications</p>
+		<div className="section">
+			<div className="card z-depth-0">
+			<div className="card-content">
+			<p>{profile.firstName} {profile.lastName}</p>
+			</div>
+			</div>
+			</div>
+
 		</div>
 		)
 }
 
+const mapStateToProps=(state)=>{
 
-export default Notifications
+	return{
+		profile:state.firebase.profile
+
+	}
+}
+export default connect(mapStateToProps)(Notifications)
